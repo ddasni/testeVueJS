499a53459a0e42d59341af303c01346532f2459b
ddasni <danielsousamelo5@gmail.com>
2025-03-06 21:40:44 -0300

    iniciando um repositorio

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h3>este é um teste de componente utilizando VueJS!!!</h3><br>
+
+    <!-- chamando aquele componente criado -->
+    <buttonTeste />
+
+  </div>
+</template>
+
+<!-- 
+o codigo que estava antes do teste
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+-->
---
+++ components/buttonTeste.vue
@@ -0,0 +1,29 @@
+<!-- estrutura do componente (HTML) -->
+<template>
+    <button class="button" @click="count++">Você clicou no botão {{ count }} vezes.</button>
+</template>
+
+
+<!-- JavaScript code -->
+<script setup>
+    // importa ref para criar variaveis reativas
+    import { ref } from 'vue'
+
+    //variavel reativa que atualiza a tela automaticamente (não precisa de F5 e etc)
+    const count = ref(0)
+</script>
+
+<!-- estilo do botão -->
+<style> 
+.button {
+  background-color: #04AA6D; /* Green */
+  border: none;
+  border-radius: 20px;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+}
+</style>
