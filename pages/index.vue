a35f540874d1120d703734da1f8c62af447c5f3d
ddasni <danielsousamelo5@gmail.com>
2025-03-08 12:59:30 -0300

    Adicionando a pagina principal

---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+      <h3>este é um teste de componente utilizando VueJS!!!</h3><br>
+  
+      <!-- chamando aquele componente criado -->
+      <buttonTeste />
+  
+    </div>
+  </template>
